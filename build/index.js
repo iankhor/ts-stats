7b9e5c9963fd5ca4a6b3afc47090518f852035d2
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-15 21:47:54 +1100

    bad code example

--- build/index.js
+++ build/index.js
@@ -1,3 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hello');
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map((row) => row.split(','));
+// console.log(matches);
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins += 1;
+    }
+    if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins += 1;
+    }
+}
+console.log(manUnitedWins);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import fs from 'fs';
 
-console.log('hello');
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// console.log(matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins += 1;
+  }
+
+  if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins += 1;
+  }
+}
+
+console.log(manUnitedWins);
