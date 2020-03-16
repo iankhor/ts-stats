ebe8ef15df83b551c9f5880e4c02b67e9853987f
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-16 23:37:50 +1100

    defining a tuple

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const utils_1 = require("./utils");
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
@@ -13,7 +14,8 @@ class CsvFileReader {
         this.data = fs_1.default
             .readFileSync(this.filename, { encoding: 'utf-8' })
             .split('\n')
-            .map((row) => row.split(','));
+            .map((row) => row.split(','))
+            .map((row) => [utils_1.dateStringToDate(row[0]), row[1], row[2], parseInt(row[3]), parseInt(row[4]), row[5], row[6]]);
     }
 }
 exports.CsvFileReader = CsvFileReader;
