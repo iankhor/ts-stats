8925aa73da5940a1f46d2b4e8f53a996c5a22e8a
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-16 23:10:37 +1100

    extract CSV reader logic into a class

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-// console.log(matches);
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins += 1;
   }
 
-  if (match[2] === 'Man United' && match[5] === 'A') {
+  if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins += 1;
   }
 }
 
-console.log(manUnitedWins);
+console.log(`Man Utd has won ${manUnitedWins} games`);
